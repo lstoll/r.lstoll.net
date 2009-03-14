0cefc06e6099dc4b4761563a4dd9f9f165a7f787
Lincoln Stoll <lstoll@lstoll.net>
2009-03-14 14:45:43 +0000

    Upgrading to newest sinatra.
    --HG--
    extra : convert_revision : d5ea8e70925e768bdf878dfff9c23c8a1791385d

--- app/runner.rb
+++ app/runner.rb
@@ -4,10 +4,10 @@ require 'sinatra'
 root_dir = File.expand_path(File.dirname(__FILE__) + "/..")
 __FILE__ == $0 ? rack = false : rack = true
 
-Sinatra::Application.default_options.merge!(
-    :views    => File.join(root_dir, 'views'),
-    :app_file => File.join(root_dir, 'app', 'application.rb'),
-    :run => rack ? false : true,
-    :env => ENV['RACK_ENV'] ? ENV["RACK_ENV"].to_sym : "development"
-)
-require File.join(root_dir, 'app', 'application.rb')
+set :root, root_dir
+set :views, File.join(root_dir, 'views')
+set :app_file, File.join(root_dir, 'app', 'application.rb')
+set :run, rack ? false : true
+set :environment, ENV['RACK_ENV'] ? ENV["RACK_ENV"].to_sym : "development"
+
+load File.join(root_dir, 'app', 'application.rb')
